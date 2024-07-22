991f72e1add2789600845cf71a0a9801b5b556d5
makkov <maxim_86nva@mail.ru>
2024-07-22 20:56:56 +0300

    quarkus-smallrye-fault-tolerance остальные паттерны

--- src/main/java/org/acme/ProblemResource.java
+++ src/main/java/org/acme/ProblemResource.java
@@ -1,10 +1,12 @@
 package org.acme;
 
+import io.smallrye.faulttolerance.api.RateLimit;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 
 @Path("/problems")
 public class ProblemResource {
@@ -22,4 +24,36 @@ public class ProblemResource {
     public String retry() {
         return problemService.retry();
     }
+
+    @GET
+    @Path("/timeout")
+    @Timeout(value = 500)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String timeout() {
+        return problemService.timeout();
+    }
+
+    @GET
+    @Path("/fallback")
+    @Timeout(value = 500)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String fallback() {
+        return problemService.fallback();
+    }
+
+    @GET
+    @Path("/circuit-breaker")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String circuitBreaker() {
+        return problemService.circuitBreaker();
+    }
+
+    @GET
+    @Path("/rate-limit")
+    @RateLimit(value = 2, window = 10)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String rateLimit() {
+        return problemService.rateLimit();
+    }
+
 }
--- src/main/java/org/acme/ProblemService.java
+++ src/main/java/org/acme/ProblemService.java
@@ -2,12 +2,66 @@ package org.acme;
 
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
 public class ProblemService {
+
+    private final Random random = new Random();
+    private final AtomicLong counter = new AtomicLong(0);
+
 
     public String retry() {
         Log.infof("Try to retry\n");
         throw new RuntimeException("Problem with retry");
     }
+    public String timeout() {
+        int timeout = random.nextInt(1000);
+        Log.infof("Will imitate timeout: %s\n", timeout);
+        try {
+            Thread.sleep(timeout);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return "Hello from service!";
+    }
+
+    @Fallback(fallbackMethod = "fallbackRecover")
+    public String fallback() {
+        boolean isWithProblem = random.nextBoolean();
+        if (isWithProblem) {
+            Log.infof("We have a some problem!");
+            throw new RuntimeException("Some problem");
+        }
+
+        return "Hello from fallback!";
+    }
+
+    public String fallbackRecover() {
+        return "Hello from fallbackRecover!";
+    }
+
+    @CircuitBreaker(requestVolumeThreshold = 4)
+    public String circuitBreaker() {
+        problemImitator();
+        return "Hello from circuitBreaker";
+    }
+
+    private void problemImitator() {
+        // introduce some artificial failures
+        final Long invocationNumber = counter.getAndIncrement();
+        Log.infof("Counter: %s, number: %s\n", counter, invocationNumber);
+        if (invocationNumber % 4 > 1) { // alternate 2 successful and 2 failing invocations
+            throw new RuntimeException("Service failed.");
+        }
+    }
+
+    public String rateLimit() {
+        return "Hello from RateLimit!";
+    }
+
 }
